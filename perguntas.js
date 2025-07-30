4819049aba8420d4202a57f718e5813b14b65745
Brunão <luppi.ribeiro@escola.pr.gov.br>
2025-07-30 14:10:02 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,43 +1,43 @@
 criaCartao(
-    'Aritmética',
-    'Quais são as operações básicas da Matemática?',
-    'Adição, Subtração, Multiplicação e Divisão.'
+    'アーサー',
+    'qual é o protagonista de red dead redemption 2?',
+    'Arthur Morgan.'
 )
 
 criaCartao(
-    'Função',
-    'O que seria o zero da função?',
-    'O valor onde o gráfico cruza o eixo da abscissa, ou ainda o valor de x para quando y for igual a 0.'
+    '市',
+    'qual é a maior cidade e red dead 2?',
+    'Saint Denis.'
 )
 
 criaCartao(
-    'Estatística',
-    'Qual a definição de média aritmética?',
-    'Resultado da divisão do somatório dos números dados pela quantidade de números somados.'
+    'アーム',
+    'qual é a arma mais forte?',
+    'Carcano Rifle.'
 )
 
 criaCartao(
-    'Geometria',
-    'Como se calcula a área de um triângulo qualquer?',
-    'Multiplique a base pela altura e divida por dois.'
+    '裏切り者',
+    'quem traio o bando?',
+    'micah.'
 )
 
 criaCartao(
-    'Conjuntos Numéricos',
-    'O que define o conjunto dos números racionais, dê exemplos?',
-    'Números que podem ser escritos na forma de fração. Por exemplo, 3/4, 10/2, -8/2'
+    '馬',
+    'qual é o cavalo mais raro?',
+    'American Paint Horse com a pelagem Splashed White'
 )
 
 criaCartao(
-    'Trigonometria',
-    'Quando utilizo o teorema de Pitágoras?',
-    'Para encontrar um dos lados de um triângulo retângulo, sendo dado os outros dois lados. h² = c² + c².'
+    '動物',
+    'quais são os animais mais fortes?',
+    'Urso-cinzento Lendário Bharati, o Bisão Tatanka Lendário e a Pantera Lendária.'
 )
 
 criaCartao(
-    'Cálculo',
-    'Qual o valor de 58 - 2 x 10?',
-    '38.'
+    '悪魔?',
+    'onde pode aparece o homem estranho?',
+    'em um espelho na cabana do pântano.'
 )
 
 criaCartao(
